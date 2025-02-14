3529c0a75f48622cec44fb454e09f709e9f23223
sotogumi <u961841f@ecs.osaka-u.ac.jp>
2025-02-14 19:14:58 +0900

    play_recently をcomponent化

---
+++ lib/components/play_recently.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class PlayRecently extends StatelessWidget {
+  const PlayRecently({
+    required this.labelText,
+    required this.imageUrl,
+    super.key,
+  });
+  final String labelText;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        width: 150,
+        height: 50,
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                imageUrl,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(labelText),
+            ),
+          ],
+        ),
+      );
+}
